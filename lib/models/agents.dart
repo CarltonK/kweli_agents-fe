537450803b3937db1c98c3e55d3524d5ef57dd66
Mark Carlton <mcarlton33@gmail.com>
2021-12-06 06:45:18 +0300

    KS-162 #comment added new fields

--- lib/models/agents.dart
+++ lib/models/agents.dart
@@ -1,14 +1,25 @@
 class AgentModel {
+  String? fullName;
+  String? phone;
   String? uid;
   String? email;
   String? password;
   DateTime? joinedOn;
 
-  AgentModel({this.email, this.password, this.uid, this.joinedOn});
+  AgentModel({
+    this.email,
+    this.password,
+    this.uid,
+    this.joinedOn,
+    this.fullName,
+    this.phone,
+  });
 
   Map<String, dynamic> toFirestore() => {
         'uid': uid,
         'email': email,
         'joinedOn': joinedOn,
+        'fullName': fullName,
+        'phone': phone,
       };
 }
