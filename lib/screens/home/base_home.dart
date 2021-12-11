b9a26aa2475bd8567a28892bab4f55e87ce18229
Mark Carlton <mcarlton33@gmail.com>
2021-12-11 08:22:22 +0300

    KS-161 #comment fix grammar error

--- lib/screens/home/base_home.dart
+++ lib/screens/home/base_home.dart
@@ -50,7 +50,7 @@ class _BaseHomeScreenState extends State<BaseHomeScreen> {
       isScrollable: true,
       tabs: [
         TabWidget(title: 'Kiosk'),
-        TabWidget(title: 'Wholesaler'),
+        TabWidget(title: 'Wholeseller'),
       ],
     );
   }
